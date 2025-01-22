a6e8b473a03dd1204ae3e0d095c1401a3ac3792b
PikuzaMarie <pikuza.mari@gmail.com>
2025-01-22 17:21:51 +0300

    config: create inputs configuration

---
+++ src/common/constants/inputsConfig/inputs-config.js
@@ -0,0 +1,22 @@
+export const INPUTS_CONFIG = [
+  {
+    label: 'initial investment',
+    type: 'number',
+    value: 0,
+  },
+  {
+    label: 'annual investment',
+    type: 'number',
+    value: 0,
+  },
+  {
+    label: 'expected return',
+    type: 'number',
+    value: 0,
+  },
+  {
+    label: 'duration',
+    type: 'number',
+    value: 0,
+  },
+];
